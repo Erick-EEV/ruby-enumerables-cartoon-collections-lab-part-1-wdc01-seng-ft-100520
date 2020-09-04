345597a343d8a572eb776cc4e3fa56c7d5624b0a
Erick Villeda <eeverickeev@gmail.com>
2020-09-04 18:58:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,9 @@ end
   # Print a custom greeting for each element
   
 def list_dwarves(array)
+  
+end
+
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
